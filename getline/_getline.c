11879b739cac933619fb307712077e607fb9543f
oladele90 <oladelemiro07@gmail.com>
2023-05-23 09:07:03 -0700

    getline revision

--- getline/_getline.c
+++ getline/_getline.c
@@ -2,55 +2,63 @@
 
 char *_getline(const int fd)
 {
-    static size_t buf_probe = 0;
-    char *temp;
-    static ssize_t read_bytes;
-    size_t total_size = 0;
-    static char r_buf[READ_SIZE];
-    char *string_form = malloc(sizeof(char) * READ_SIZE);
-    for (; buf_probe < READ_SIZE && read_bytes == READ_SIZE; buf_probe++)
-    {
-        if (r_buf[buf_probe] == '\n')
-        {
-            memset(&r_buf[buf_probe], '\0', 1);
-            memset(&string_form[total_size], r_buf[buf_probe], 1);
-            total_size += 1;
-            return (string_form);
-        }
-        if (r_buf[buf_probe] != '\0')
-        {
-            memset(&string_form[total_size], r_buf[buf_probe], 1);
-            total_size += 1;
-        }
-    }
-    read_bytes = read(fd, r_buf, READ_SIZE);
-    if (read_bytes <= 0)
-            return (NULL);
-    
-    while (!strstr(r_buf, "\n") && read_bytes == READ_SIZE)
-    {
-        temp = realloc(string_form, total_size + read_bytes + 1);
-        string_form = temp;
-        memcpy(string_form + total_size, r_buf, read_bytes);
-        total_size += read_bytes;
-        read_bytes = read(fd, r_buf, READ_SIZE);
-    }
-    temp = realloc(string_form, total_size + read_bytes + 1);
-    string_form = temp;
-    for (buf_probe = 0; buf_probe < READ_SIZE; buf_probe++)
-    {
-        if (r_buf[buf_probe] == '\n')
-        {
-            memset(&r_buf[buf_probe], '\0', 1);
-            memset(&string_form[total_size], r_buf[buf_probe], 1);
-            total_size += 1;
-            return (string_form);
-        }
-        if (r_buf[buf_probe] != '\0')
-        {
-            memset(&string_form[total_size], r_buf[buf_probe], 1);
-            total_size += 1;
-        }
-    }
-    return (string_form);
+	static char buffer[READ_SIZE];
+	static ssize_t buffer_index;
+	static ssize_t read_bytes;
+
+	char *line = NULL;
+	size_t line_length = 0;
+
+	while (1)
+	{
+		if (buffer_index >= read_bytes)
+		{
+			buffer_index = 0;
+			read_bytes = read(fd, buffer, READ_SIZE);
+			if (read_bytes <= 0)
+			{
+				if (line_length == 0)
+					return (NULL);
+
+				char *temp = realloc(line, line_length + 1);
+
+				if (!temp)
+				{
+					free(line);
+					return (NULL);
+				}
+				line = temp;
+				line[line_length] = '\0';
+				return (line);
+			}
+		}
+		while (buffer_index < read_bytes)
+		{
+			if (buffer[buffer_index] == '\n')
+			{
+				char *temp = realloc(line, line_length + 1);
+
+				if (!temp)
+				{
+					free(line);
+					return (NULL);
+				}
+				line = temp;
+				line[line_length] = '\0';
+				buffer_index++;
+				return (line);
+			}
+				char *temp = realloc(line, line_length + 1);
+
+				if (!temp)
+				{
+					free(line);
+					return (NULL);
+				}
+				line = temp;
+				line[line_length] = buffer[buffer_index];
+				line_length++;
+				buffer_index++;
+		}
+	}
 }
