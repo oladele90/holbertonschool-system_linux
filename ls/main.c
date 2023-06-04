5e52e0e9ad4088985d7cb9bcff388adfb06e9c6f
oladele90 <oladelemiro07@gmail.com>
2023-06-04 09:07:59 -0700

    fix iteration print

--- ls/main.c
+++ ls/main.c
@@ -48,10 +48,11 @@ int main(int argc, char **argv)
 			entryInfos = sortStruct(entryInfos);
 			selectPrint(entryInfos, flag);
 			for (x = 0; entryInfos[x].entry != NULL; x++)
+			{
 				free(entryInfos[i].entry);
-			free(entryInfos);
-			printf("\n");
-		}
+				free(entryInfos);
+				printf("\n");
+			}
 	}
 	return (0);
 }
