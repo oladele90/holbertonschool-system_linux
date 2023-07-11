df8071fa1a34ff10b3a171fbbc7d8908d28c71c2
oladele90 <oladelemiro07@gmail.com>
2023-07-11 20:00:57 +0000

    adds match option

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -6,12 +6,11 @@ asm_strncmp:
 xor rax, rax
 mov rbx, rdi
 mov rcx, rsi
-mov rdx, rdx
 
 
 loop_start:
 cmp rdx, 0
-je end
+je match
 mov al, byte [rbx]
 mov dl, byte [rcx]
 cmp al, 0
@@ -21,27 +20,35 @@ je sec_null
 cmp al, dl
 jl less_than
 jg greater_than
+dec rdx
+cmp rdx, 0
+je match
 inc rbx
 inc rcx
-dec rdx
 jmp loop_start
 
 first_null:
 cmp al, dl
-jne less_than
-je end
+je match
+jmp less_than
 
 sec_null:
 cmp al, dl
-jne greater_than
-je end
+je match
+jmp greater_than
+
+match:
+xor rax, rax
+jmp end
 
 less_than:
-mov rax, 1
+xor rax, rax
+inc rax
 jmp end
 
 greater_than:
-mov rax, -1
+xor rax, rax
+dec rax
 jmp end
 
 end:
