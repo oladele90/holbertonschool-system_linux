942e8f6373f9649885c4dbe9237215f13299df12
oladele90 <oladelemiro07@gmail.com>
2023-07-11 21:04:46 +0000

    fixes wrong logic and register usage

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -3,52 +3,45 @@ BITS 64
 global asm_strncmp
 
 asm_strncmp:
-xor rax, rax
-mov rbx, rdi
-mov rcx, rsi
-
 
 loop_start:
 cmp rdx, 0
-je match
-mov al, byte [rbx]
-mov dl, byte [rcx]
-cmp al, 0
+je matbh
+cmp [rdi], byte 0
 je first_null
-cmp dl, 0
+cmp [rsi], byte 0
 je sec_null
-cmp al, dl
+mov bl, byte [rdi]
+mov bh, byte [rsi]
+cmp bl, bh
 jl less_than
 jg greater_than
+inc rdi
+inc rsi
 dec rdx
-cmp rdx, 0
-je match
-inc rbx
-inc rcx
 jmp loop_start
-
-first_null:
-cmp al, dl
-je match
-jmp less_than
-
-sec_null:
-cmp al, dl
-je match
-jmp greater_than
-
-match:
+matbh:
 xor rax, rax
 jmp end
 
+first_null:
+cmp [rsi], byte 0
+jne less_than
+je end
+
+sec_null:
+cmp [rdi], byte 0 
+jne greater_than
+je end
+
 less_than:
 xor rax, rax
-inc rax
+mov rax, -1
 jmp end
 
 greater_than:
 xor rax, rax
-dec rax
+mov rax, 1
 jmp end
 
 end:
