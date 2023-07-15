a72bf48c7b9d81b41680a1c23213d7b312544071
oladele90 <oladelemiro07@gmail.com>
2023-07-15 09:04:46 +0000

    strcasecmp imp

---
+++ libasm/8-strcasecmp.asm
@@ -0,0 +1,51 @@
+BITS 64
+
+global asm_strcasecmp
+
+section .text
+asm_strcasecmp:
+
+loop_start:
+mov rax, rdi
+mov rdx, rsi
+movzx eax, BYTE [rax]
+movzx edx, BYTE [rdx]
+cmp al, 0
+jne null_checker
+cmp dl, 0
+je return_calc
+null_checker:
+cmp al, 65
+jl switch_lower
+cmp al, 90
+jg switch_lower
+add ax, 32
+switch_lower:
+cmp dl, 65
+jl increment
+cmp dl, 90
+jg increment
+add dx, 32
+
+increment:
+cmp al, dl
+jne return_calc
+inc rdi
+inc rsi
+jmp loop_start
+return_calc:
+cmp al, dl
+je equal
+jl less
+sub al, dl
+jmp end
+equal:
+mov rax, 0
+jmp end
+less:
+sub al, dl
+neg al
+imul eax, -1
+jmp end
+end:
+ret
