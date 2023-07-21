728f8e8a635d7449483936ee64f1bea3c2324208
oladele90 <oladelemiro07@gmail.com>
2023-07-21 14:58:52 +0000

    SIGINT handler

---
+++ signals/0-handle_signal.c
@@ -0,0 +1,23 @@
+#include "signals.h"
+
+/**
+ * signal_handler - prints message when SIGINT signal is sent
+ * @sig: current signal
+ * Return: none
+*/
+void signal_handler(int sig)
+{
+	printf("Gotcha! [%d]\n", sig);
+}
+
+/**
+ * handle_signal - signal handler for SIGINT
+ * Return: -1 for error/ 0 for success
+*/
+int handle_signal(void)
+{
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+		return (-1);
+
+	return (0);
+}
---
+++ signals/signals.h
@@ -0,0 +1,12 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int handle_signal(void);
+#endif
