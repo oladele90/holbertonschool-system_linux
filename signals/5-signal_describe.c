c037039cda6363f71e7951816af0b3b88a816e7a
oladele90 <oladelemiro07@gmail.com>
2023-07-25 23:18:40 +0000

    strsignal()

--- signals/5-signal_describe.c
+++ signals/5-signal_describe.c
@@ -6,13 +6,15 @@
  * @argv: An array of strings representing the command-line arguments.
  * Return: 0 on success, or an appropriate error code on failure.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
 		printf("Usage: %s <signum>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
 	printf("%s: %s\n", argv[1], strsignal(atoi(argv[1])));
+
 	return (0);
 }
