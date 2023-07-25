c40b9c688f6f32cef53d9aa0172531e696702a6c
oladele90 <oladelemiro07@gmail.com>
2023-07-25 13:43:20 +0000

    adds betty documentation

--- signals/4-trace_signal_sender.c
+++ signals/4-trace_signal_sender.c
@@ -1,18 +1,28 @@
 #include "signals.h"
 
+/**
+ * handle_sig - Signal handler function for SIGQUIT.
+ * @sig: The signal number caught (unused).
+ */
+
 void handle_sig(int sig)
 {
-    (void)sig;
+	(void)sig;
 	printf("SIGQUIT sent by %d\n", getppid());
 	fflush(stdout);
 }
 
+/**
+ * trace_signal_sender - Set a signal handler for SIGQUIT using sigaction.
+ * Return: On success, returns 0. On error, returns -1.
+ */
+
 int trace_signal_sender(void)
 {
 	struct sigaction sa;
+
 	sa.sa_handler = handle_sig;
 
 	return (sigaction(SIGQUIT, &sa, NULL));
-
 }
 
