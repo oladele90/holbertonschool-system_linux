75089f0c67303114f2fc52fa0344af2e4b55eb58
oladele90 <oladelemiro07@gmail.com>
2023-08-07 14:25:10 +0000

    bytes

---
+++ 0x08_CPython/2-python.c
@@ -0,0 +1,42 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_bytes(PyObject *p)
+{
+    int i;
+    int size = (((PyVarObject*)(p))->ob_size);
+    printf("[.] bytes object info\n");
+    printf("  size: %d\n", size);
+    printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
+    if (size > 9)
+        size = 10;
+    else
+        size++;
+    printf("  first %d bytes: ", size);
+    for (i = 0; i < size; i++)
+    {
+        printf("%02hhx", (((PyBytesObject*)(p))->ob_sval[i]));
+        if (i == size - 1)
+            printf("\n");
+        else
+            printf(" ");
+        
+    }
+}
+
+void print_python_list(PyObject *p)
+{
+    int size = (((PyVarObject*)(p))->ob_size);
+    int alloc = (((PyListObject*)(p))->allocated);
+    printf("[*] Python list info\n[*] Size of the Python List = %d\n[*] Allocated = %d\n", size, alloc);
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        PyObject *item = (((PyListObject*)(p))->ob_item[i]);
+        printf("Element %d: %s\n", i, PyList_GetItem(p, i)->ob_type->tp_name);
+        if (PyBytes_Check(item))
+        {
+            print_python_bytes(item);
+        }
+    }
+}
