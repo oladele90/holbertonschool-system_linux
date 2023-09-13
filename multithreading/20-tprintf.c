20524538da4e5bb4dda3f6e19dbfba9934979781
oladele90 <oladelemiro07@gmail.com>
2023-09-13 02:49:51 +0000

    adds constructor and destructor

--- multithreading/20-tprintf.c
+++ multithreading/20-tprintf.c
@@ -1,16 +1,28 @@
 #include "multithreading.h"
 
-#include "multithreading.h"
-
+pthread_mutex_t lock;
+void begin (void) __attribute__((constructor));
+void end (void) __attribute__((destructor));
 /**
  * tprintf - prints formatted string
  * @format: string to print
  * Return: 0
 */
+void begin(void)
+{
+	pthread_mutex_init(&lock, NULL);
+}
+
+void end(void)
+{
+	pthread_mutex_destroy(&lock);
+}
 int tprintf(char const *format, ...)
 {
 	pthread_t tid = pthread_self();
 
+	pthread_mutex_lock(&lock);
 	printf("[%lu] %s", tid, format);
+	pthread_mutex_unlock(&lock);
 	return (0);
 }
