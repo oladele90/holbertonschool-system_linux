7ddc41af54f8b1a58fb0dbd310a6bc1ba0dc48c4
oladele90 <oladelemiro07@gmail.com>
2023-09-14 16:38:38 +0000

    handles va_args

--- multithreading/20-tprintf.c
+++ multithreading/20-tprintf.c
@@ -1,8 +1,8 @@
 #include "multithreading.h"
 
 pthread_mutex_t lock;
-void begin (void) __attribute__((constructor));
-void end (void) __attribute__((destructor));
+void begin(void) __attribute__((constructor));
+void end(void) __attribute__((destructor));
 /**
  * tprintf - prints formatted string
  * @format: string to print
@@ -20,9 +20,13 @@ void end(void)
 int tprintf(char const *format, ...)
 {
 	pthread_t tid = pthread_self();
+	va_list args;
 
 	pthread_mutex_lock(&lock);
-	printf("[%lu] %s", tid, format);
+	va_start(args, format);
+	printf("[%lu]", tid);
+	vprintf(args, format);
+	va_end(args);
 	pthread_mutex_unlock(&lock);
 	return (0);
 }
