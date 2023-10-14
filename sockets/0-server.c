4abe70f5ff7a691fd6a20f2746b1245022702aaf
oladele90 <oladelemiro07@gmail.com>
2023-10-14 21:27:17 +0000

    fixes betty

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -6,19 +6,23 @@
 #include "stdio.h"
 #include "string.h"
 
+/**
+* main - creates binds and listens to a socket
+* Return: 1
+*/
 
 int main(void)
 {
-    int sockid;
-    struct sockaddr_in *addrport = calloc(1, sizeof(struct sockaddr_in));
+	int sockid;
+	struct sockaddr_in *addrport = calloc(1, sizeof(struct sockaddr_in));
 
-    sockid = socket(PF_INET, SOCK_STREAM, 0);
-    addrport->sin_family = AF_INET;
-    addrport->sin_addr.s_addr = INADDR_ANY;
-    addrport->sin_port = htons(12345);
-    bind(sockid, (struct sockaddr *)addrport, sizeof(struct sockaddr_in));
-    printf("server listening on port 12345\n");
-    while (1)
-        listen(sockid, 8);
-    return (1);
+	sockid = socket(PF_INET, SOCK_STREAM, 0);
+	addrport->sin_family = AF_INET;
+	addrport->sin_addr.s_addr = INADDR_ANY;
+	addrport->sin_port = htons(12345);
+	bind(sockid, (struct sockaddr *)addrport, sizeof(struct sockaddr_in));
+	printf("server listening on port 12345\n");
+	while (1)
+		listen(sockid, 8);
+	return (1);
 }
--- sockets/1-server.c
+++ sockets/1-server.c
@@ -6,25 +6,29 @@
 #include "stdio.h"
 #include "string.h"
 
+/**
+* main - creates binds listens and connects to a socket
+* Return: 1
+*/
 
 int main(void)
 {
-    int sockid;
-    struct sockaddr_in *client = calloc(1, sizeof(struct sockaddr_in));
-    struct sockaddr_in *addrport = calloc(1, sizeof(struct sockaddr_in));
-    socklen_t *new = 0;
+	int sockid;
+	struct sockaddr_in *client = calloc(1, sizeof(struct sockaddr_in));
+	struct sockaddr_in *addrport = calloc(1, sizeof(struct sockaddr_in));
+	socklen_t *new = 0;
 
-    sockid = socket(PF_INET, SOCK_STREAM, 0);
-    addrport->sin_family = AF_INET;
-    addrport->sin_addr.s_addr = INADDR_ANY;
-    addrport->sin_port = htons(12345);
-    bind(sockid, (struct sockaddr *)addrport, sizeof(struct sockaddr_in));
-    printf("server listening on port 12345\n");
-    while (listen(sockid, 8) == 0)
-    {
-        accept(sockid, (struct sockaddr *)client ,new);
-        printf("Client connected: %s\n", inet_ntoa(client->sin_addr));
-        break;
-    }
-    return (1);
+	sockid = socket(PF_INET, SOCK_STREAM, 0);
+	addrport->sin_family = AF_INET;
+	addrport->sin_addr.s_addr = INADDR_ANY;
+	addrport->sin_port = htons(12345);
+	bind(sockid, (struct sockaddr *)addrport, sizeof(struct sockaddr_in));
+	printf("server listening on port 12345\n");
+	while (listen(sockid, 8) == 0)
+	{
+		accept(sockid, (struct sockaddr *)client, new);
+		printf("Client connected: %s\n", inet_ntoa(client->sin_addr));
+		break;
+	}
+	return (1);
 }
