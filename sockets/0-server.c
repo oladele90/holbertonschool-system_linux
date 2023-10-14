86f881fc6a600a932f2ec96e8d25b5b0dd77150f
oladele90 <oladelemiro07@gmail.com>
2023-10-14 19:54:14 +0000

    listening on port 12345

---
+++ sockets/0-server.c
@@ -0,0 +1,24 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+
+int main(void)
+{
+    int sockid;
+    struct sockaddr_in *addrport = calloc(1, sizeof(struct sockaddr_in));
+
+    sockid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    addrport->sin_family = AF_INET;
+    addrport->sin_addr.s_addr = htonl(INADDR_ANY);
+    addrport->sin_port = htons(12345);
+    bind(sockid, (struct sockaddr *) addrport, sizeof(addrport));
+    printf("server listening on port 12345\n");
+    while (1)
+        listen(sockid, 8);
+    return (1);
+}
