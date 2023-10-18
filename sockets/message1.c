d5ac8d68e44ea899e4b9df42140a3d0b653c4cd2
oladele90 <oladelemiro07@gmail.com>
2023-10-18 22:02:28 +0000

    adding new line

--- sockets/message1.c
+++ sockets/message1.c
@@ -2,11 +2,13 @@
 
 void requst_breakdown_printout(void *message)
 {
-    char *path = NULL, *line = NULL;
+    char *path = NULL, *line = NULL, *method = NULL;
     char *brk = "\r\n";
 
     line = strtok(message, brk);
-    path = strtok(line, " ");
+    method = strtok(line, " ");
+    path = strtok(NULL, "?");
+    printf("Method = %s\n", method);
     printf("%s\n", path);
     fflush(stdout);
 }
