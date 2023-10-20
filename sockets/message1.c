06505d5ecb40db5e54f26db7b8a232470ec79eb4
oladele90 <oladelemiro07@gmail.com>
2023-10-20 21:55:08 +0000

    todo 1

--- sockets/message1.c
+++ sockets/message1.c
@@ -2,19 +2,22 @@
 
 void requst_breakdown_printout(void *message)
 {
-    char *path = NULL, *line = NULL, *method = NULL, *all_data = NULL;
-    char *brk = "\r\n", *req_type = NULL;
+    char *path = NULL, *line = NULL, *method = NULL, *all_data = NULL, *data = NULL;
+    char *brk = "\r\n", *req_type = NULL, *save_ptr = NULL, *key = NULL, *value = NULL;
 
     line = strtok(message, brk);
     method = strtok(line, " ");
     path = strtok(NULL, "?");
     all_data = strtok(NULL, " ");
     req_type = strtok(NULL, "\r\n");
-
-
-    printf("Method: %s\n", method);
     printf("Path: %s\n", path);
-    printf("Data: %s\n", all_data);
-    printf("Request type: %s\n", req_type);
-    fflush(stdout);
+    data = strtok_r(all_data, brk, &save_ptr);
+    while(data && method && req_type)
+    {
+        key = strtok(data, "=");
+        value = strtok(NULL, "=");
+        printf("Query: \"%s\" -> \"%s\"", key, value);
+        data = strtok_r(NULL, brk, &save_ptr);
+    }
+    return;
 }
