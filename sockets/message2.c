d9355e7571a97397584bee7732ac7de5e8664d20
oladele90 <oladelemiro07@gmail.com>
2023-10-23 19:52:17 +0000

    checking placement of linme after strtok

--- sockets/message2.c
+++ sockets/message2.c
@@ -2,14 +2,16 @@
 
 void requst_breakdown_printout(void *message)
 {
-    char *line = NULL, *user_agent_ptr = NULL, *user = NULL;
-    char *brk = "\r\n";
+    char *line = NULL, *user_agent_ptr = NULL, *host = NULL;
+    char *brk = "\r\n", *user;
 
     line = strtok_r(message, brk, &user_agent_ptr);
+    host = strtok_r(NULL, brk, &user_agent_ptr);
     user = strtok_r(NULL, brk, &user_agent_ptr);
 
     printf("%s\n", line);
     printf("%s\n", user);
+    printf("%s\n", host);
 }
 /*
 Raw request: "GET /test HTTP/1.1
