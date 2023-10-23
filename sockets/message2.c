a9dfa6e3494cd2f8410346e4c8ec4de1e3e9a4d4
oladele90 <oladelemiro07@gmail.com>
2023-10-23 20:48:11 +0000

    checking placement of linme after strtok

--- sockets/message2.c
+++ sockets/message2.c
@@ -2,14 +2,16 @@
 
 void requst_breakdown_printout(void *message)
 {
-    char *line = NULL, *user_agent_ptr = NULL;
+    char *line = NULL, *user_agent_ptr = NULL, *key = NULL, *value = NULL;
     char *brk = "\r\n";
 
     line = strtok_r(message, brk, &user_agent_ptr);
     line = strtok_r(NULL, brk, &user_agent_ptr);
     while (line)
     {
-        printf("%s\n", line);
+        key = strtok(line, ":");
+        value = strtok(NULL, "\t");
+        printf("%s -> %s\n", key, value);
         line = strtok_r(NULL, brk, &user_agent_ptr);
     }
 
